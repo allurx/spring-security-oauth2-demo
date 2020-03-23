904c5003f9773402f784cef61d36bf5eff58c80b
zyc <1343786337@qq.com>
2020-03-23 22:57:58 +0800

    Initial commit

---
+++ src/test/java/com/zyc/security/oauth2/SpringSecurityOauth2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyc.security.oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauth2DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
