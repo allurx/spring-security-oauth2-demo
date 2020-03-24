b1b48b9548b0fc1d9e6fec24d6c76e744ec27be4
zyc <1343786337@qq.com>
2020-03-24 23:59:49 +0800

    code review

---
+++ spring-security-oauth2-client/src/main/java/red/zyc/spring/security/oauth2/client/SpringSecurityOauth2ClientApplication.java
@@ -0,0 +1,16 @@
+package red.zyc.spring.security.oauth2.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zyc
+ */
+@SpringBootApplication
+public class SpringSecurityOauth2ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityOauth2ClientApplication.class, args);
+    }
+
+}
---
+++ spring-security-oauth2-client/src/main/java/red/zyc/spring/security/oauth2/client/config/WebSecurityConfig.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package red.zyc.spring.security.oauth2.client.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+/**
+ * @author zyc
+ */
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeRequests(a -> a
+                        .antMatchers("/", "index.html", "/error").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .oauth2Login().and()
+                .logout(l -> l.logoutSuccessUrl("/").permitAll())
+                .csrf().disable();
+
+    }
+}
