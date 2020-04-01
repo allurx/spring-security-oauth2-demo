04577ed70f0044bbcd76a563f9624ddf77114348
zyc <1343786337@qq.com>
2020-04-01 23:06:27 +0800

    添加资源服务器demo

---
+++ spring-security-oauth2-resourceserver/src/main/java/red/zyc/spring/security/oauth2/resourceserver/SpringSecurityOauth2ResourceServerApplication.java
@@ -0,0 +1,16 @@
+package red.zyc.spring.security.oauth2.resourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zyc
+ */
+@SpringBootApplication
+public class SpringSecurityOauth2ResourceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityOauth2ResourceServerApplication.class, args);
+    }
+
+}
